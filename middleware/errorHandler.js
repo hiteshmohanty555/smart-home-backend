799aba3bd8f8a853c89a781b34df9da9b95081ad
hiteshmohanty555 <hiteshmohanty555@gmail.com>
2026-03-04 20:44:38 +0530

    Remove duplicate middleware folder from root

--- middleware/errorHandler.js
+++
@@ -1,6 +0,0 @@
-// middleware/errorHandler.js
-function errorHandler(err, req, res, next) {
-  console.error(err);
-  res.status(500).json({ error: "Internal server error" });
-}
-module.exports = errorHandler;
